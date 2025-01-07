f459e26304342cbf0714e98fc3d4160d16406d37
cstroh23 <146174848+cstroh23@users.noreply.github.com>
2025-01-07 15:26:34 +0100

    Big Main Menu Update, also changed  SampleScene to Semester1Scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    public void PlayGame() {
-        SceneManager.LoadScene("SampleScene"); //For the moment only calling Level 1
+    public void PlayGameSemester1() {
+        SceneManager.LoadScene("Semester1Scene");
+    }
+    public void PlayGameSemester2() {
+        SceneManager.LoadScene("Semester2Scene");
+    }
+    public void PlayGameSemester3() {
+        SceneManager.LoadScene("Semester3Scene");
+    }
+    public void PlayGameSemester4() {
+        SceneManager.LoadScene("Semester4Scene");
+    }
+    public void PlayGameSemester5() {
+        SceneManager.LoadScene("Semester5Scene");
+    }
+    public void PlayGameSemester6() {
+        SceneManager.LoadScene("Semester6Scene");
     }
 
     public void GoToSettingsMenu() {
@@ -19,4 +34,19 @@ public class MainMenu : MonoBehaviour {
     public void QuitGame() {
         Application.Quit();
     }
+
+    public void GoToChapterMenu() {
+        SceneManager.LoadScene("ChapterScene");
+    }
+
+    public void GoToChapter1Menu() {
+        SceneManager.LoadScene("Chapter1Scene");
+    }
+    public void GoToChapter2Menu() {
+        SceneManager.LoadScene("Chapter2Scene");
+    }
+    public void GoToChapter3Menu() {
+        SceneManager.LoadScene("Chapter3Scene");
+    }
+    
 }
